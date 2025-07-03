5d63e2c1d3ef7f38a0772e527808c741a4c91f53
sam2023-code <sam335211@gmail.com>
2025-07-03 12:20:37 +0800

    v703_5_1220

--- src/navbar_pages/Home.jsx
+++ src/navbar_pages/Home.jsx
@@ -1,5 +1,13 @@
+import logo_Home_Jason from '../assets/Home_Jason.png'
+import '../css/Home.css';
+
 function Home() { 
-  return <><h1>Home</h1></>;
+  return (
+    <>
+      <h1>Home</h1>
+      <img src={logo_Home_Jason} alt="" className='logo_Home_Jason' />
+    </>
+  );
 }
 
 export default Home
--- src/navbar_pages/Useful_link.jsx
+++ src/navbar_pages/Useful_link.jsx
@@ -3,15 +3,27 @@ import '../css/style.css'; // Make sure to import your CSS file
 const Useful_link = () => {
   return (
     <>
+    <br/>
     <h3 style={{ textAlign: 'center' }}>Useful_link</h3>
     
-	
+	<div class="single_row">
+		<a href="https://us-east-1.console.aws.amazon.com/console/home?region=us-east-1">
+		AWS Console
+		</a>
+	</div>
+	<br/>
+
+    
 	<div class="single_row">
 		<a href="https://hub.docker.com/repositories/samchan930423">
 		hub docker
 		</a>
 	</div>
 	<br/>
+
+
+
+
 	
     Youtube:
     <div class="single_row">
@@ -21,20 +33,6 @@ const Useful_link = () => {
 	</div>
 	<br/>
 
-	<div class="single_row">
-		<a href="https://www.youtube.com/watch?v=vj8KSZjPTUU&ab_channel=%E8%B5%B0%E6%AD%AA%E7%9A%84%E5%B7%A5%E7%A8%8B%E5%B8%ABJames">
-			React vite project
-		</a>
-	</div>
-	<br/>
-	
-	<div class="single_row">
-		<a href="https://www.youtube.com/watch?v=5mO-T2o9zuk&ab_channel=GreatStack">
-			React Navbar
-		</a>
-	</div>
-	<br/>
-	
 	<div class="single_row">
 		<a href="https://www.youtube.com/watch?v=p6xDCz00TxU&t=428s&ab_channel=TechWorldwithNana">
 			EKS tutorial
