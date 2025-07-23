e67d64750aa8e889efe60ee91391b7172f1431ad
sam2023-code <sam335211@gmail.com>
2025-07-23 18:09:57 +0800

    v723_2_1809

--- src/config.jsx
+++ src/config.jsx
@@ -1,7 +1,7 @@
 //export const API_BASE_URL = 'http://ec2-44-204-173-147.compute-1.amazonaws.com:8080';
 //xport const API_BASE_URL = 'http://localhost:8080';
 export const API_BASE_URL =  location.protocol + "//" + location.hostname + ":8443"
-//export const API_BASE_URL =  "https://52.0.45.228" + ":8443"
+//export const API_BASE_URL =  "https://www.jasonpakpak0423.com:8443"
 export const API_BASE_TEXT =  location.protocol + "//" + location.hostname + ":8080"
 export const DEBUG_MODE = true;
 export const APP_VERSION = '1.0.0';
