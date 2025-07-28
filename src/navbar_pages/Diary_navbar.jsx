23f077f462923f993a76b8b235db6a3d55c84c05
sam2023-code <sam335211@gmail.com>
2025-07-28 13:10:44 +0800

    v728_2_1310

--- src/navbar_pages/Diary_navbar.jsx
+++ src/navbar_pages/Diary_navbar.jsx
@@ -1,20 +1,32 @@
 // Navbar.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../css/diary_navbar.css'; // Import CSS for styling
 
+
 const Diary_navbar = () => {
+  
   return (
     <div className="diary-navbar">
       <h2></h2>
       <ul>
-        <li><Link to="/diary/item_bicycle">第一次踏單車</Link></li>
-        <li><Link to="/diary/item_juice">自己飲果汁</Link></li>
-        <li><Link to="/diary/item_ferry">第一次坐船</Link></li>
-        <li><Link to="/diary/item_jumpin">第一次波波池</Link></li>
+        <li><NavLink to="/diary/item_bicycle" style={({ isActive }) => (isActive? styles.actLink: styles.link)} >第一次踏單車</NavLink></li>
+        <li><NavLink to="/diary/item_juice" style={({ isActive }) => (isActive? styles.actLink: styles.link)} >自己飲果汁</NavLink></li>
+        <li><NavLink to="/diary/item_ferry" style={({ isActive }) => (isActive? styles.actLink: styles.link)} >第一次坐船</NavLink></li>
+        <li><NavLink to="/diary/item_jumpin" style={({ isActive }) => (isActive? styles.actLink: styles.link)} >第一次波波池</NavLink></li>
       </ul>
     </div>
   );
 };
 
 export default Diary_navbar;
+
+const styles = {
+
+  link: {
+    backgroundColor: 'white'
+  },
+  actLink: {
+    backgroundColor: 'lightblue', // Change this color as needed
+  },
+};
